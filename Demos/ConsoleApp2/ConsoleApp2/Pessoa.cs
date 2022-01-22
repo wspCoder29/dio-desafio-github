b1dc893387817ca48214fa3f1485b936424aecb5
LeandroFerreira <leandro.wsp@gmail.com>
2022-01-22 19:42:55 -0300

    Testes com vetores

---
+++ Demos/ConsoleApp2/ConsoleApp2/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Documento { get; set; }
+
+        public static void AlterarNome(Pessoa p, string novoNome)
+        {
+            p.Nome = novoNome;
+        }
+
+
+
+
+
+
+
+
+    }
+}
