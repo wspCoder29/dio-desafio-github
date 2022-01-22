d98f34b1be5b1c093cd86ce5390f8e9f29dbfc01
LeandroFerreira <leandro.wsp@gmail.com>
2022-01-22 19:10:59 -0300

    Demos de Reference Types

---
+++ Demos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Testando Reference Types
+
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Sonic";
+            p1.Idade = 30;
+            p1.Documento = "432";
+
+            WriteLine($"Nome inicial: {p1.Nome}");
+
+            Pessoa.AlterarNome(p1,"Knucles");
+
+            WriteLine($"Nome inicial: {p1.Nome}");
+
+
+
+
+
+        }
+    }
+}
