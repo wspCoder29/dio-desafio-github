675b7e031a1f385d212ea7c06e8ee06ee910329b
LeandroFerreira <leandro.wsp@gmail.com>
2022-01-24 23:02:28 -0300

    Adicionei Projeto sobre: Construtores, Delegates, Propriedades

---
+++ Construtores/Construtores/Models/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores.Models
+{
+    public class Calculadora
+    {
+
+        //Delagate
+        public delegate void DelegateCalculadora();
+
+        //Evento
+        public static event DelegateCalculadora EventoCalculadora;
+
+
+        public static void Somar(int x, int y)
+        {
+
+            if (EventoCalculadora != null)
+            {
+                System.Console.WriteLine($"Adição: {x + y}");
+                EventoCalculadora();
+            }
+            else
+            {
+                Console.WriteLine("Nenhum inscrito");
+            }
+            Console.WriteLine($"Adição: {x+y}");
+        }
+
+        public static void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"Subtração: {x - y}");
+        }
+
+
+
+
+
+    }
+}
---
+++ Construtores/Construtores/Program.cs
@@ -0,0 +1,58 @@
+﻿using Construtores.Models;
+using System;
+
+namespace Construtores
+{
+    class Program
+    {
+
+     public delegate void Operacao(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+
+
+
+            /*
+            Operacao op = new Operacao(Calculadora.Somar);
+            op += Calculadora.Subtrair;
+            op.Invoke(10,10);
+            */
+
+            /*
+            Data data = new Data();
+            //data.SetMes(2);
+
+            data.Mes = 20;
+            //Console.WriteLine(data.Mes);
+            data.ApresentarMes();
+            */
+
+            /*
+            Aluno p1 = new Aluno("Leandro", "Ferreira", "Teste");
+            p1.Apresentar();
+            */
+
+            //  Pessoa p1 = new Pessoa("Leandro", "Ferreira");
+            //  p1.Apresentar();
+
+            /*
+            Log log = Log.GetInstance();
+
+            log.PropriedadeLog = "Teste Instancia";
+
+            Log log2 = Log.GetInstance();
+            Console.WriteLine(log2.PropriedadeLog);
+            */
+
+
+
+
+
+        }
+    }
+}
