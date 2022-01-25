16bdcce8101e6ef9085351fcd844c5b25b0b7b11
LeandroFerreira <leandro.wsp@gmail.com>
2022-01-24 23:06:01 -0300

    Adicionei projeto sobre: Construtores, Delegates, Propriedades

---
+++ Construtores/Construtores/Models/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores.Models
+{
+    class Pessoa
+    {
+
+        private string nome;
+        private string sobrenome;
+
+        /*
+        public Pessoa()
+        {
+            nome = string.Empty;
+            sobrenome = string.Empty;
+        }
+        */
+
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            Console.WriteLine("Construtor classe Pessoa");
+        }
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é: {nome} {sobrenome}");
+        }
+
+
+
+
+
+
+
+    }
+}
