ff68b87ec3b006604b451a9cda37b185d7690475
LeandroFerreira <leandro.wsp@gmail.com>
2022-01-25 16:34:24 -0300

    Projeto para teste de movimentação de arquivos entre diretorios

---
+++ Directory_and_DirectoryInfo/Program.cs
@@ -0,0 +1,111 @@
+﻿CriarDiretoriosGlobo();
+//CriarArquivo();
+
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory,
+                                                        "globo",
+                                                        "América do Sul",
+                                                        "Argentina",
+                                                        "brasil.txt");
+
+
+//MoverArquivo(origem, destino);
+CopiarArquivo(origem, destino);
+
+static void CopiarArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo de origem não existe");
+        return;
+    }
+
+    if (File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo já existe na pasta de destino");
+        return;
+    }
+    File.Move(pathOrigem, pathDestino);
+
+    File.Copy(pathOrigem, pathDestino);
+
+}
+
+
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo de origem não existe");
+        return;
+    }
+
+    if (File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo já existe na pasta de destino");
+        return;
+    }
+    File.Move(pathOrigem, pathDestino);
+}
+
+
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: 233mm");
+        sw.WriteLine("IDH: 0,901");
+        sw.WriteLine("Dados atualizados em: 25/01/2022");
+    }
+}
+
+
+
+static void CriarDiretoriosGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    if (!Directory.Exists(path))
+    {
+        var dirGlobo = Directory.CreateDirectory(path);
+        var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+        dirAmNorte.CreateSubdirectory("USA");
+        dirAmNorte.CreateSubdirectory("Mexico");
+        dirAmNorte.CreateSubdirectory("Canada");
+
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmCentral.CreateSubdirectory("Panamá");
+
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguai");
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
